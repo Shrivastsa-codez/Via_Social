b9dca072228cd384ca916d8bb92741eeb863ae9c
Shrivastsa-codez <78452805+Shrivastsa-codez@users.noreply.github.com>
2021-12-31 12:56:39 -0600

    Add files via upload

--- room.js
+++ room.js
@@ -10,11 +10,39 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML=" Welcome to Via Social" + user_name + "!";
   
 function logout(){
     window.location="index.html";
 }
 
 function addroom(){
+  room_name=document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+        purpose: "adding a room name"
+  });
+
+  localStorage.setItem("room_name", room_name);
+  window.location="vs_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+      console.log("room name-" + Room_names);
+      row="<div class='room_name' id="+  Room_names + " onclick='redirectToRoomName(this.id)'> #" + Room_names + "</div> <hr>";
+      document.getElementById("output").innerHTML+=row;
+
+//End code
+});});}
+getData();
+
+function redirectToRoomName(name){
+  console.log(name);
+  localStorage.setItem("room_name", name);
+  window.location="vs_page.html";
 }
